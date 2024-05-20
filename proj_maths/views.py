023926a57576e1cea73d9e6465c0bbf716729a7a
pc <ip.antonova62@gmail.com>
2024-05-20 17:53:46 +0300

    Проведена работа с pylint

--- proj_maths/views.py
+++ proj_maths/views.py
@@ -114,30 +114,6 @@ def send_term(request):
         add_term(request)
 
 
-def send_translate(request):
-    """ функция записи переводов в файл CSV"""
-    if request.method == "POST":
-        cache.clear()
-        user_name = request.POST.get("name")
-        new_term = request.POST.get("new_term1", "")
-        new_definition = request.POST.get("new_definition1", "").replace(";", ",")
-        context = {"user": user_name}
-        if len(new_definition) == 0:
-            context["success"] = False
-            context["comment"] = "Описание должно быть не пустым"
-        elif len(new_term) == 0:
-            context["success"] = False
-            context["comment"] = "Термин должен быть не пустым"
-        else:
-            context["success"] = True
-            context["comment"] = "Ваш термин принят"
-            terms_work.write_translate(new_term1, new_definition1)
-        if context["success"]:
-            context["success-title"] = ""
-        return render(request, "term_request.html", context)
-    else:
-        add_term(request)
-
 
 def show_stats(request):
     """ функция показа статистики"""
